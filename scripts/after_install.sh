32979287fa13c62c0a20dfa551a564cbec088810
Nimy Mathew <nimy@dexlock.com>
2023-01-27 16:31:07 +0530

    scripts added

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,3 +2,4 @@
 cd /home/ec2-user/
 npm install
 npm install --save react react-dom react-scripts react-particles-js
+npm install pm2 -g
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/server/
-npm start
+pm2 start npm --name "react-aws-deploy" -- start
+pm2 startup
+pm2 save
+pm2 restart all
